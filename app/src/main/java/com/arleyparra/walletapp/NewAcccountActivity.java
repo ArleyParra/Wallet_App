868daee8409aec8065e7dfe87b1f5283c055a4d1
Arley <aparra269@unab.edu.co>
2023-08-30 18:01:43 -0500

    adelanto de clase

---
+++ app/src/main/java/com/arleyparra/walletapp/NewAcccountActivity.java
@@ -0,0 +1,14 @@
+package com.arleyparra.walletapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewAcccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_acccount);
+    }
+}
